fc7bf0c29f6f4ab4871cd18fc30e3690c0e905f6
William Redenbaugh <wmredenbaugh@gmail.com>
2023-10-01 23:19:26 -0700

    pimeroni matrix implementation

---
+++ pimeroni_matrix.cpp
@@ -0,0 +1,93 @@
+#include "global_includes.h"
+#include "pimeroni_matrix.h"
+
+static const int PIMERONI_BYTES_PER_PIXEL = 3;
+static const int PIMERONI_WIDTH = 16;
+static const int PIMERONI_HEIGHT = 16;
+
+static const uint8_t PIMERONI_SOF = 0x72;
+
+typedef struct _os_pimeroni_t
+{
+    os_spi_t *spi_bus;
+    os_device_t device;
+    uint8_t output_buffer[PIMERONI_BYTES_PER_PIXEL * PIMERONI_WIDTH * PIMERONI_HEIGHT + 1];
+} _os_pimeroni_t;
+
+static int os_pimeroni_init(void *ptr, int width, int height)
+{
+
+    _os_pimeroni_t *pimeroni = (_os_pimeroni_t *)ptr;
+    if (pimeroni == NULL)
+    {
+        return OS_RET_NULL_PTR;
+    }
+
+    pimeroni->output_buffer[0] = PIMERONI_SOF;
+
+    os_device_init_params init_params;
+
+    os_device_init_params params = {
+        .dma_buf_size = PIMERONI_BYTES_PER_PIXEL * PIMERONI_WIDTH * PIMERONI_HEIGHT + 1,
+        .spi_mode = 0,
+        .cs_gpio = GPIO_NUM_9,
+        .clk = 2000000,
+        .bus = pimeroni->spi_bus,
+    };
+
+    int ret = os_spi_couple_device(init_params, &pimeroni->device);
+    return OS_RET_OK;
+}
+
+static int os_pimeroni_set(void *ptr, int x, int y, uint8_t r, uint8_t g, uint8_t b)
+{
+    if (x >= 16 | y >= 16)
+    {
+        return OS_RET_INVALID_PARAM;
+    }
+
+    int pos = ((PIMERONI_WIDTH * y + x)) * 3 + 1;
+
+    _os_pimeroni_t *pimeroni = (_os_pimeroni_t *)ptr;
+    if (pimeroni == NULL)
+    {
+        return OS_RET_NULL_PTR;
+    }
+
+    pimeroni->output_buffer[pos] = r;
+    pimeroni->output_buffer[pos + 1] = g;
+    pimeroni->output_buffer[pos + 2] = b;
+
+    return OS_RET_OK;
+}
+
+static int os_pimeroni_update(void *ptr)
+{
+
+    _os_pimeroni_t *pimeroni = (_os_pimeroni_t *)ptr;
+    if (pimeroni == NULL)
+    {
+        return OS_RET_NULL_PTR;
+    }
+
+    const size_t data_sent_out = sizeof(uint8_t) * PIMERONI_BYTES_PER_PIXEL * PIMERONI_WIDTH * PIMERONI_HEIGHT + 1;
+    int ret = os_spi_send(&pimeroni->device, pimeroni->output_buffer, data_sent_out);
+    return OS_RET_OK;
+}
+
+int os_init_pimeroni_matrix(os_ledmatrix_t *matrix, os_spi_t *spi_bus)
+{
+
+    os_ledmatrix_init_t init_params;
+
+    init_params.height = 16;
+    init_params.width = 16;
+    init_params.init_func = os_pimeroni_init;
+    init_params.setpixel_func = os_pimeroni_set;
+    init_params.update_func = os_pimeroni_update;
+
+    init_params.matrix_ptr = (void *)malloc(sizeof(_os_pimeroni_t));
+
+    int ret = os_init_ledmatrix(init_params, matrix);
+    return ret;
+}
---
+++ pimeroni_matrix.h
@@ -0,0 +1,7 @@
+#ifndef _PIMERONI_MATRIX_H
+#define _PIMERONI_MATRIX_H
+#include "CHAL_SHARED/csal_ledmatrix.h"
+
+int os_init_pimeroni_matrix(os_ledmatrix_t *matrix, os_spi_t *spi_bus);
+
+#endif
